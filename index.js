7cd5725094553dfc7e9901778545567b583bf5bc
joshuasorkin <joshua.d.sorkin@gmail.com>
2024-02-17 15:19:57 -0800

    remove .replace

--- index.js
+++ index.js
@@ -12,11 +12,6 @@ app.get('/api/new-route', async (req, res) => {
         // Retrieve score and language from the request
         const origin = req.query.origin;
         const destination = req.session.destination;
-        
-
-        // Initialize the base URL
-        let url = process.env.GOOGLE_MAP_URL.replace("[ORIGIN]",origin).replace("[DESTINATION]",destination);
-
 
         // Array to hold query parameters
         const queryParams = [];
@@ -33,7 +28,7 @@ app.get('/api/new-route', async (req, res) => {
 
         // If there are any query parameters, append them to the URL
         if (queryParams.length > 0) {
-            url += '?' + queryParams.join('&');
+            url += '&' + queryParams.join('&');
         }
 
         console.log({url});
@@ -50,37 +45,7 @@ app.get('/api/new-route', async (req, res) => {
     }
 });
 
-
-// Proxy endpoint for check-game
-app.get('/api/check-game', async (req, res) => {
-    try {
-        const { gameId, playerSolution } = req.query;
-        const url = process.env.BACK_END_URI+`/check-game?gameId=${gameId}&playerSolution=${playerSolution}`;
-        console.log({url});
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const data = await response.json();
-        console.log({data});
-        res.json(data); // Send the response back to the frontend
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).json({ error: error.message });
-    }
-});
-
 app.use(express.static('public'));
-
-app.get('/language/:language', (req,res) => {
-    req.session.language = req.params.language;
-    res.sendFile(path.join(__dirname,'public','index.html'));
-});
-
-
-
-
-
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
